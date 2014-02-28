d6647a1483d3efec418bf47a61e94a98536a0a06
ragingwind <ragingwind@gmail.com>
2014-02-28 22:33:13 +0900

    Add autocopy option

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,5 +1,32 @@
 'use strict';
 
+function showOptions() {
+  document.getElementsByClassName('options')[0].style.display = 'block';
+}
+
+function copyToClipboard() {
+  var url = document.getElementById('url');
+  var color = url.style.color;
+  var textRange = document.createRange();
+  var sel;
+
+  textRange.selectNode(url);
+  sel = window.getSelection();
+  sel.addRange(textRange);
+  document.execCommand("Copy");
+  sel.removeAllRanges();
+
+  url.style.color = 'red';
+  setTimeout(function() {
+    url.style.color = color;
+  }, 300);
+}
+
+function setAutocopyText(on) {
+  on = (on ? 'off' : 'on');
+  document.getElementById('autocopy').innerText = 'autocopy is ' + on;
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
   var queryOpts = {currentWindow: true, active: true};
   var port = chrome.runtime.connect({name: "shortern"});
@@ -10,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
         path: icon
       }, function() {
         setTimeout(function() {
-          cb(tab)
+          cb(tab);
         }, 100);
       });
     });
@@ -22,7 +49,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
                .replace('{{title}}', tab[0].title)
                .replace('{{shortenUrl}}', res.shortUrl);
 
-      document.getElementById('url').innerText = text;;
+      document.getElementById('url').innerText = text;
+      localforage.getItem('autocopy', function(val) {
+        setAutocopyText(val);
+        val && copyToClipboard();
+        showOptions();
+      });
     });
   });
 
@@ -33,21 +65,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
   });
 
   document.getElementsByClassName('outter')[0].addEventListener("click", function() {
-    var textRange = document.createRange();
-    var url = document.getElementById('url');
-    var sel;
+    copyToClipboard();
+  });
 
-    textRange.selectNode(url);
-    sel = window.getSelection();
-    sel.addRange(textRange);
-    document.execCommand("Copy");
-    sel.removeAllRanges();
-
-    var color = url.style.color;
-    url.style.color = 'red';
-
-    setTimeout(function() {
-      url.style.color = color;
-    }, 300);
+  document.getElementById('autocopy').addEventListener("click", function() {
+    localforage.getItem('autocopy', function(val) {
+      val = !val;
+      setAutocopyText(val);
+      localforage.setItem('autocopy', val);
+    });
   });
 });
